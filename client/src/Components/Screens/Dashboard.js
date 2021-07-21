9720db695d34b986ffa8ea55344d9a210d59d9f3
rajranjan0608 <rranjan01234@gmail.com>
2021-07-21 19:54:49 +0530

    integrated client side with drizzle api calls

--- client/src/Components/Screens/Dashboard.js
+++ client/src/Components/Screens/Dashboard.js
@@ -1,182 +1,264 @@
-import { useEffect } from 'react';
-import { Table, MetaMaskButton } from 'rimble-ui';
-import { Link } from 'react-router-dom';
-import '../styles/Layout.scss';
-import '../styles/Dashboard.scss';
-import { COLORS } from '../constants';
-import CreateElectionModal from './modals/CreateElectionModal';
-import Navbar from './Navbar';
-import { useDrizzleContext } from '../../drizzle/drizzleContext';
-import { useUserContext } from '../../ReducerComponents/Context/UserContext';
+import { useEffect, useContext, useMemo } from "react";
+import { Table, MetaMaskButton } from "rimble-ui";
+import { Link } from "react-router-dom";
+import "../styles/Layout.scss";
+import "../styles/Dashboard.scss";
+import { COLORS } from "../constants";
+import CreateElectionModal from "./modals/CreateElectionModal";
+import Navbar from "./Navbar";
+import { useCallContext } from "../../drizzle/calls";
 
-function Dashboard() {
-	const { drizzle, drizzleVariables } = useDrizzleContext();
-	const { userInfo } = useUserContext();
+const Dashboard = () => {
+  // const { userInfo } = useUserContext();
 
-	const CardItem = ({headerValue, descriptor, imgUrl, imgBackground = "#f7f7f7"}) => {
-		return (
-			<div className="shadow cardItem">
-				<div className="centered">
-					<div className="cardImageHolder" style={{backgroundColor: imgBackground}}>
-						<div className="centered">
-							<img src={imgUrl} className="cardImage" alt="profile-pic"/>
-						</div>
-					</div>
+  const {
+    MainContract,
+    UserContract,
+    userInfo,
+    electionDetails,
+    UserSubscriber,
+  } = useCallContext();
 
-					<font size = "2" className="cardText">
-						<font size="3">{headerValue}</font>
-						<span className="text-muted">{descriptor}</span>
-					</font>
-				</div>
-			</div>
-		)
-	}
+  return useMemo(() => {
+    const CardItem = ({
+      headerValue,
+      descriptor,
+      imgUrl,
+      imgBackground = "#f7f7f7",
+    }) => {
+      return (
+        <div className="shadow cardItem">
+          <div className="centered">
+            <div
+              className="cardImageHolder"
+              style={{ backgroundColor: imgBackground }}
+            >
+              <div className="centered">
+                <img src={imgUrl} className="cardImage" alt="profile-pic" />
+              </div>
+            </div>
 
-	const Status = ({status, text}) => {
-		return (
-			<div>
-				<div className="status">
-					<div className="statusIndicator" style={{backgroundColor: COLORS[status]}}></div>
-					<font>{text}</font>
-				</div>
-			</div>
-		)
-	}
+            <font size="2" className="cardText">
+              <font size="3">{headerValue}</font>
+              <span className="text-muted">{descriptor}</span>
+            </font>
+          </div>
+        </div>
+      );
+    };
 
-	const ElectionRow = ({electionId, electionTitle, electionAddress, startDate, endDate, status}) => {
-		return (
-			<tr style={{cursor: "pointer"}} onClick={()=> {window.location.href='/election'}}>
-				<td>{electionId}</td>
-				<td className="tableDetails">
-					<font>{electionTitle}</font><br/>
-					<font className="text-muted" size="1">{electionAddress}</font>
-				</td>
-				<td>{startDate}</td>
-				<td>{endDate}</td>
-				<td>
-					<Status status={status} text={status.charAt(0).toUpperCase() + status.slice(1)}/>
-				</td>
-			</tr>
-		)
-	}
+    const Status = ({ status, text }) => {
+      return (
+        <div>
+          <div className="status">
+            <div
+              className="statusIndicator"
+              style={{ backgroundColor: COLORS[status] }}
+            ></div>
+            <font>{text}</font>
+          </div>
+        </div>
+      );
+    };
 
-	return (
-		<div style={{backgroundColor: "#f7f7f7", minHeight: "100%"}}>
-			<Navbar header={userInfo?.name} infoText={userInfo?.publicAddress} pictureUrl="/assets/avatar.png"/>
+    const ElectionRow = ({
+      electionId,
+      electionTitle,
+      electionAddress,
+      startDate,
+      endDate,
+      status,
+    }) => {
+      return (
+        <tr
+          style={{ cursor: "pointer" }}
+          onClick={() => {
+            window.location.href = `/election?contractAddress=${electionAddress}`;
+          }}
+        >
+          <td>{electionId}</td>
+          <td className="tableDetails">
+            <font>{electionTitle}</font>
+            <br />
+            <font className="text-muted" size="1">
+              {electionAddress}
+            </font>
+          </td>
+          <td>{startDate}</td>
+          <td>{endDate}</td>
+          <td>
+            <Status
+              status={status}
+              text={status.charAt(0).toUpperCase() + status.slice(1)}
+            />
+          </td>
+        </tr>
+      );
+    };
 
-			<div style={{margin: "30px"}}>
-				<div style={{width: "100%"}}>
-					<div style={{float: "left"}}>
-						<h4 style={{marginBottom: "0px"}}>Dashboard</h4>
-						<font size="2" className="text-muted">Agora Blockchain</font>
-					</div>
+    return (
+      <div style={{ backgroundColor: "#f7f7f7", minHeight: "100%" }}>
+        <Navbar header={userInfo?.name} infoText={userInfo?.contractAddress} pictureUrl="/assets/avatar.png"/>
 
+        <div style={{ padding: "30px"}}>
+          <div style={{ width: "100%" }}>
+            <div style={{ float: "left" }}>
+              <h4 style={{ marginBottom: "0px" }}>Dashboard</h4>
+              <font size="2" className="text-muted">
+                Agora Blockchain
+              </font>
+            </div>
 
-					<div style={{float: "right"}}>
-						<CreateElectionModal/>
-					</div>
-				</div>
+            <div style={{ float: "right" }}>
+              <CreateElectionModal UserContract={UserContract} account={userInfo?.publicAddress}/>
+            </div>
+          </div>
 
-				<br/><br/><br/>
+          <br />
+          <br />
+          <br />
 
-				<div className="cardContainer row">
-					<CardItem headerValue="4" descriptor="Total elections" imgUrl="/assets/totalElections.png"/>
+          <div className="cardContainer row">
+            <CardItem
+              headerValue="4"
+              descriptor="Total elections"
+              imgUrl="/assets/totalElections.png"
+            />
 
-					<CardItem headerValue="2" descriptor="Active elections" imgUrl="/assets/activeElections.png" imgBackground="#eaffe8"/>
+            <CardItem
+              headerValue="2"
+              descriptor="Active elections"
+              imgUrl="/assets/activeElections.png"
+              imgBackground="#eaffe8"
+            />
 
-					<CardItem headerValue="1" descriptor="Closed elections" imgUrl="/assets/endedElections.png" imgBackground="#ffe8e8"/>
+            <CardItem
+              headerValue="1"
+              descriptor="Closed elections"
+              imgUrl="/assets/endedElections.png"
+              imgBackground="#ffe8e8"
+            />
 
-					<CardItem headerValue="1" descriptor="Pending elections" imgUrl="/assets/pendingElections.png" imgBackground="#fffbd1"/>
-				</div>
+            <CardItem
+              headerValue="1"
+              descriptor="Pending elections"
+              imgUrl="/assets/pendingElections.png"
+              imgBackground="#fffbd1"
+            />
+          </div>
 
-				<div className="layoutBody row">
-					<div className="lhsLayout">
-						<div className="lhsHeader" style={{marginTop: "10px"}}>
-							<h5>Elections</h5>
-						</div>
+          <div className="layoutBody row">
+            <div className="lhsLayout">
+              <div className="lhsHeader" style={{ marginTop: "10px" }}>
+                <h5>Elections</h5>
+              </div>
 
-						<br/>
-						
-						<div className="lhsBody">
-							<Table>
-								<thead>
-									<tr>
-									<th>ID</th>
-									<th>Details</th>
-									<th>Start Date</th>
-									<th>End Date</th>
-									<th>Status</th>
-									</tr>
-								</thead>
-								<tbody style={{fontSize: "13px"}}>
-									<ElectionRow
-										electionId = "1" 
-										electionTitle = "Test Election"
-										electionAddress = "0xF30F9801df6c722C552Fd60E8E201A4c0524BFAb"
-										startDate = "Monday, 7th June 2021"
-										endDate = "Monday, 14th June 2021"
-										status = "active"
-									/>
+              <br />
 
-									<ElectionRow
-										electionId = "1" 
-										electionTitle = "Test Election"
-										electionAddress = "0xF30F9801df6c722C552Fd60E8E201A4c0524BFAb"
-										startDate = "Monday, 7th June 2021"
-										endDate = "Monday, 14th June 2021"
-										status = "pending"
-									/>
+              <div className="lhsBody">
+                <Table>
+                  <thead>
+                    <tr>
+                      <th>ID</th>
+                      <th>Details</th>
+                      <th>Start Date</th>
+                      <th>End Date</th>
+                      <th>Status</th>
+                    </tr>
+                  </thead>
+                  <tbody style={{ fontSize: "13px" }}>
+                    {
+                      electionDetails?.map((election) => (
+                      	<ElectionRow
+                      		electionId = {election?.info?.id}
+                      		electionTitle = {election?.info?.name}
+                      		electionAddress = {election?.contractAddress}
+                      		startDate = {(new Date(election?.info?.sdate * 1)).toLocaleString()}
+                      		endDate = {(new Date(election?.info?.edate * 1)).toLocaleString()}
+                      		status = "active"
+						/>
+					  ))
+                    }
 
-									<ElectionRow
-										electionId = "1" 
-										electionTitle = "Test Election"
-										electionAddress = "0xF30F9801df6c722C552Fd60E8E201A4c0524BFAb"
-										startDate = "Monday, 7th June 2021"
-										endDate = "Monday, 14th June 2021"
-										status = "closed"
-									/>
-								</tbody>
-							</Table>
-						</div>
-					</div>
+                    <ElectionRow
+                      electionId="1"
+                      electionTitle="Test Election"
+                      electionAddress="0xF30F9801df6c722C552Fd60E8E201A4c0524BFAb"
+                      startDate="Monday, 7th June 2021"
+                      endDate="Monday, 14th June 2021"
+                      status="pending"
+                    />
 
-					<div className="rhsLayout">
-						<div className="lhsHeader" style={{marginTop: "10px"}}>
-							<h5>Network Information</h5>
-						</div>
+                    <ElectionRow
+                      electionId="1"
+                      electionTitle="Test Election"
+                      electionAddress="0xF30F9801df6c722C552Fd60E8E201A4c0524BFAb"
+                      startDate="Monday, 7th June 2021"
+                      endDate="Monday, 14th June 2021"
+                      status="closed"
+                    />
+                  </tbody>
+                </Table>
 
-						<hr/>
+                <br />
+                <br />
+              </div>
+            </div>
 
-						<div className="networkHeader">
-							<div className="imageHolder shadow" style={{backgroundColor: "#f7f7f7"}}>
-								<div className="centered">
-									<img src="/assets/ethereum.png" className="image" alt="profile-pic"/>
-								</div>
-							</div>
+            <div className="rhsLayout">
+              <div className="lhsHeader" style={{ marginTop: "10px" }}>
+                <h5>Network Information</h5>
+              </div>
 
-							<font size = "2" className="imageText">
-								<font size="3">Current Network</font>
-								<Status status="active" text="Ethereum Mainnet"/>
-							</font>
-						</div>
+              <hr />
 
-						<br/>
+              <div className="networkHeader">
+                <div
+                  className="imageHolder shadow"
+                  style={{ backgroundColor: "#f7f7f7" }}
+                >
+                  <div className="centered">
+                    <img
+                      src="/assets/ethereum.png"
+                      className="image"
+                      alt="profile-pic"
+                    />
+                  </div>
+                </div>
 
-						<h4 style={{marginBottom: "0px"}}>0.04 ETH</h4>
-						<font size="2" className="text-muted">0x9505...c8E49</font>
+                <font size="2" className="imageText">
+                  <font size="3">Current Network</font>
+                  <Status status="active" text="Ethereum Mainnet" />
+                </font>
+              </div>
 
-						<div style={{display: "flex", height: "190px", flexDirection: "row", alignItems: "flex-end"}}>
-							<MetaMaskButton.Outline style={{width: "100%"}}>
-								Connect
-							</MetaMaskButton.Outline>
-						</div>
-					</div>
-				</div>
+              <br />
 
-				<br/>
-			</div>
-		</div>
-	)
-}
+              <h4 style={{ marginBottom: "0px" }}>0.04 ETH</h4>
+              <font size="2" className="text-muted">
+                0x9505...c8E49
+              </font>
+
+              <div
+                style={{
+                  display: "flex",
+                  height: "190px",
+                  flexDirection: "row",
+                  alignItems: "flex-end",
+                }}
+              >
+                <MetaMaskButton.Outline style={{ width: "100%" }}>
+                  Connect
+                </MetaMaskButton.Outline>
+              </div>
+            </div>
+          </div>
+
+          <br />
+        </div>
+      </div>
+    );
+  }, [UserContract, userInfo, electionDetails]);
+};
 
 export default Dashboard;
--- client/src/Components/Screens/Election.js
+++ client/src/Components/Screens/Election.js
@@ -1,5 +1,6 @@
 import { useTimer } from 'react-timer-hook';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useMemo } from 'react';
+import { useLocation } from "react-router-dom";
 import '../styles/Election.scss';
 import '../styles/Layout.scss';
 import VoteModal from './modals/VoteModal';
@@ -7,123 +8,130 @@ import DeleteModal from './modals/DeleteModal';
 import Candidate from './modals/Candidate'
 import Navbar from './Navbar';
 import TimerStyled from './TimerStyled';
+import { useCallContext } from "../../drizzle/calls";
 
 function Election() {
     const [isAdmin, setAdmin] = useState(true);
     const [isActive, setActive] = useState(true);
-    const [isPending, setPending] = useState(true);
+	const [isPending, setPending] = useState(true);
 
-	const CardItem = ({headerValue, descriptor, imgUrl, imgBackground = "#f7f7f7"}) => {
+	const search = useLocation().search;
+	const contractAddress = new URLSearchParams(search).get('contractAddress');
+
+	const { getCurrentElection, CurrentElection, currentElectionDetails, userInfo } = useCallContext();
+	console.log(currentElectionDetails);
+
+	getCurrentElection(contractAddress);
+
+	return useMemo(() => {
+		const CardItem = ({headerValue, descriptor, imgUrl, imgBackground = "#f7f7f7"}) => {
+			return (
+				<div className="shadow cardItem">
+					<div className="centered">
+						<div className="cardImageHolder" style={{backgroundColor: imgBackground}}>
+							<div className="centered">
+								<img src={imgUrl} className="cardImage" alt="profile-pic"/>
+							</div>
+						</div>
+
+						<font size = "2" className="cardText">
+							<font size="2">{headerValue}</font>
+							<span className="text-muted">{descriptor}</span>
+						</font>
+					</div>
+				</div>
+			)
+		}
+
+		const MyTimer = ({ expiryTimestamp }) => {
+			const {
+				seconds,
+				minutes,
+				hours,
+				days,
+				start
+			} = useTimer({ expiryTimestamp, onExpire: () => console.warn('onExpire called'), autostart: "false"});
+			useEffect(() => {
+			    start();
+			}, [])
+			return (
+				<TimerStyled seconds={seconds} minutes={minutes} hours={hours} days={days} />
+			);
+		}      
+
 		return (
-			<div className="shadow cardItem">
-				<div className="centered">
-					<div className="cardImageHolder" style={{backgroundColor: imgBackground}}>
-						<div className="centered">
-							<img src={imgUrl} className="cardImage" alt="profile-pic"/>
+			<div style={{backgroundColor: "#f7f7f7", minHeight: "100%"}}>
+				<Navbar header={userInfo?.name} infoText={userInfo?.contractAddress} pictureUrl="/assets/avatar.png"/>
+				<div style={{padding: "30px"}}>
+					<div style={{width: "100%"}}>
+						<div style={{float: "left"}}>
+							<h5 style={{marginBottom: "0px"}}>{currentElectionDetails?.info?.name}</h5>
+							<font className="text-muted" size="2">Ayush Tiwari</font><br/>
+							{/* <font size="1" className="text-muted" style={{marginTop: "0px"}}>0xF30F9801df6c722C552Fd60E8E201A4c0524BFAb</font> */}
+						</div>
+
+
+						<div style={{float: "right", display: "flex"}}>
+							<MyTimer expiryTimestamp={currentElectionDetails?.info?.edate} />
+							<DeleteModal Candidate={Candidate} isAdmin={isAdmin} isPending={isPending}/>
+							<VoteModal Candidate={Candidate} isActive={isActive} isPending={isPending}/>
 						</div>
 					</div>
 
-					<font size = "2" className="cardText">
-						<font size="2">{headerValue}</font>
-						<span className="text-muted">{descriptor}</span>
-					</font>
+					<br/><br/><br/>
+
+					<div className="cardContainer row">
+						<CardItem headerValue={currentElectionDetails?.info?.algorithm} descriptor="Algorithm" imgUrl="/assets/totalElections.png"/>
+
+						<CardItem headerValue={(new Date(currentElectionDetails?.info?.sdate * 1)).toLocaleString()} descriptor="Start date" imgUrl="/assets/activeElections.png" imgBackground="#eaffe8"/>
+
+						<CardItem headerValue={(new Date(currentElectionDetails?.info?.sdate * 1)).toLocaleString()} descriptor="End date" imgUrl="/assets/endedElections.png" imgBackground="#ffe8e8"/>
+
+						<CardItem headerValue="137" descriptor="Total voters" imgUrl="/assets/pendingElections.png" imgBackground="#fffbd1"/>
+					</div>
+
+					<div className="layoutBody row">
+						<div className="lhsLayout" style={{overflowY: "scroll"}}>
+							<div className="lhsHeader" style={{marginTop: "10px"}}>
+								<h5>Election Details</h5>
+							</div>
+							
+							<div className="lhsBody" style={{textAlign: "justify"}}>
+								<font size="2" className="text-muted">
+									<p>{currentElectionDetails?.info?.description}</p>
+								</font>
+
+								<br/>
+
+								<h5>About {currentElectionDetails?.info?.algorithm} Algorithm</h5>
+								<font size="2" className="text-muted">
+									The Boyer-Moore voting algorithm is one of the popular optimal algorithms which is used to find the majority element among the given elements that have more than N/ 2 occurrences. This works perfectly fine for finding the majority element which takes 2 traversals over the given elements, which works in O(N) time complexity and O(1) space complexity.   
+								</font>
+							</div>
+						</div>
+
+						<div className="rhsLayout" style={{overflowY: "scroll"}}>
+							<div className="lhsHeader" style={{marginTop: "10px"}}>
+								<h5>Candidates (4)</h5>
+							</div>
+
+							<br/>
+
+							<Candidate name="Raj" id="1" imageUrl="/assets/avatar.png" modalEnabled="true"/>
+
+							<Candidate name="Ayush" id="2" imageUrl="/assets/avatar2.png" modalEnabled="true"/>
+
+							<Candidate name="Thuva" id="3" imageUrl="/assets/avatar4.png" modalEnabled="true"/>
+
+							<Candidate name="Bruno" id="4" imageUrl="/assets/avatar3.png" modalEnabled="true"/>
+						</div>
+					</div>
+
+					<br/>
 				</div>
 			</div>
 		)
-	}
-
-    const MyTimer = ({ expiryTimestamp }) => {
-        expiryTimestamp = new Date(expiryTimestamp).getTime()
-        console.log(expiryTimestamp)
-        const {
-          seconds,
-          minutes,
-          hours,
-          days,
-          start
-        } = useTimer({ expiryTimestamp, onExpire: () => console.warn('onExpire called'), autostart: "false"});
-        // useEffect(() => {
-        //     start();
-        // })
-        return (
-            <TimerStyled seconds={seconds} minutes={minutes} hours={hours} days={days} />
-        );
-    }      
-
-	return (
-		<div style={{backgroundColor: "#f7f7f7", minHeight: "100%"}}>
-			<Navbar header="Raj Ranjan" infoText="0xF30F9801df6c722C552Fd60E8E201A4c0524BFAb" pictureUrl="/assets/avatar.png"/>
-
-			<div style={{margin: "30px"}}>
-				<div style={{width: "100%"}}>
-					<div style={{float: "left"}}>
-						<h5 style={{marginBottom: "0px"}}>My First Election</h5>
-                        <font className="text-muted" size="2">Ayush Tiwari</font><br/>
-						{/* <font size="1" className="text-muted" style={{marginTop: "0px"}}>0xF30F9801df6c722C552Fd60E8E201A4c0524BFAb</font> */}
-					</div>
-
-
-					<div style={{float: "right", display: "flex"}}>
-                        <MyTimer expiryTimestamp="2021-06-16 14:47:00" />
-                        <DeleteModal Candidate={Candidate} isAdmin={isAdmin} isPending={isPending}/>
-						<VoteModal Candidate={Candidate} isActive={isActive} isPending={isPending}/>
-					</div>
-				</div>
-
-				<br/><br/><br/>
-
-				<div className="cardContainer row">
-					<CardItem headerValue="Moore's" descriptor="Algorithm" imgUrl="/assets/totalElections.png"/>
-
-					<CardItem headerValue="Monday, 7th June 2021" descriptor="Start date" imgUrl="/assets/activeElections.png" imgBackground="#eaffe8"/>
-
-					<CardItem headerValue="Monday, 14th June 2021" descriptor="End date" imgUrl="/assets/endedElections.png" imgBackground="#ffe8e8"/>
-
-					<CardItem headerValue="137" descriptor="Total voters" imgUrl="/assets/pendingElections.png" imgBackground="#fffbd1"/>
-				</div>
-
-				<div className="layoutBody row">
-					<div className="lhsLayout" style={{overflowY: "scroll"}}>
-						<div className="lhsHeader" style={{marginTop: "10px"}}>
-							<h5>Election Details</h5>
-						</div>
-						
-						<div className="lhsBody" style={{textAlign: "justify"}}>
-                            <font size="2" className="text-muted">
-                                <p>Elections have been the usual mechanism by which modern representative democracy has operated since the 17th century. Elections may fill offices in the legislature, sometimes in the executive and judiciary, and for regional and local government. This process is also used in many other private and business organisations, from clubs to voluntary associations and corporations.</p>
-                                <p>The universal use of elections as a tool for selecting representatives in modern representative democracies is in contrast with the practice in the democratic archetype, ancient Athens, where the Elections were not used were considered an oligarchic institution and most political offices were filled using sortition, also known as allotment, by which officeholders were chosen by lot.</p>
-                            </font>
-
-                            <br/>
-
-                            <h5>About Moore's Algorithm</h5>
-                            <font size="2" className="text-muted">
-                                The Boyer-Moore voting algorithm is one of the popular optimal algorithms which is used to find the majority element among the given elements that have more than N/ 2 occurrences. This works perfectly fine for finding the majority element which takes 2 traversals over the given elements, which works in O(N) time complexity and O(1) space complexity.   
-                            </font>
-                        </div>
-					</div>
-
-					<div className="rhsLayout" style={{overflowY: "scroll"}}>
-						<div className="lhsHeader" style={{marginTop: "10px"}}>
-							<h5>Candidates (4)</h5>
-						</div>
-
-                        <br/>
-
-                        <Candidate name="Raj" id="1" imageUrl="/assets/avatar.png" modalEnabled="true"/>
-
-                        <Candidate name="Ayush" id="2" imageUrl="/assets/avatar2.png" modalEnabled="true"/>
-
-                        <Candidate name="Thuva" id="3" imageUrl="/assets/avatar4.png" modalEnabled="true"/>
-
-                        <Candidate name="Bruno" id="4" imageUrl="/assets/avatar3.png" modalEnabled="true"/>
-					</div>
-				</div>
-
-				<br/>
-			</div>
-		</div>
-	)
+	}, [CurrentElection, userInfo])
 }
 
 export default Election;
