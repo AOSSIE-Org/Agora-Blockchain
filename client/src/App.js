816fa9beed4c7c2f6b05738382d29a9e2a5e7105
rajranjan0608 <rranjan01234@gmail.com>
2021-07-21 19:05:33 +0530

    drizzle api context setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,11 @@
 import { BrowserRouter } from "react-router-dom";
 import Routing from "./Routing";
 import "./App.css";
-import ElectionContext from "./ReducerComponents/Context/ElectionContext";
-import { UserProvider } from "./ReducerComponents/Context/UserContext";
 
 function App() {
   return (
     <BrowserRouter>
-      <UserProvider>
-        <ElectionContext>
-          <Routing/>
-        </ElectionContext>
-      </UserProvider>
+      <Routing/>
     </BrowserRouter>
   );
 }
