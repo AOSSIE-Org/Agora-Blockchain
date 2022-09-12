2e0228a59ff9358b592dc83dea0849de616561cf
rajranjan0608 <rranjan01234@gmail.com>
2022-09-12 18:05:50 +0530

    merge develop

---
+++ client/src/interaction/abis/index.js
@@ -0,0 +1,7 @@
+const MainContract = require('./MainContract.json')
+const ElectionOrganizer = require('./ElectionOrganizer.json')
+
+export const ContractABI = {
+    MainContract,
+    ElectionOrganizer
+}
