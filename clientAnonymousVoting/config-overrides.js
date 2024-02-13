ce037d53cfd17b24e735d489618a9bb16bfc15f9
Aditya Bhattad <93488388+adityabhattad2021@users.noreply.github.com>
2024-02-13 17:25:11 +0000

    Fix module not found error by integrating polyfills with react-rewired

---
+++ clientAnonymousVoting/config-overrides.js
@@ -0,0 +1,17 @@
+const webpack = require('webpack');
+module.exports = function override(config) {
+  const fallback = config.resolve.fallback || {};
+  Object.assign(fallback, {
+    crypto: require.resolve('crypto-browserify'),
+    stream: require.resolve('stream-browserify'),
+    worker_threads: false,
+  });
+  config.resolve.fallback = fallback;
+  config.plugins = (config.plugins || []).concat([
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+      Buffer: ['buffer', 'Buffer'],
+    }),
+  ]);
+  return config;
+};
