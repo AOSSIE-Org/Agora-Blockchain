f1453467ef2fdbbc5e98737c5414bb07d32b6f6c
Aditya Bhattad <93488388+adityabhattad2021@users.noreply.github.com>
2024-02-21 10:37:57 +0000

    add new contract addresses

---
+++ clientAnonymousVoting/src/constants/contractAddresses.js
@@ -0,0 +1,4 @@
+
+    export const oneVoteAddress = "0x2bd5008dbb949aDF563A781000a7B2858F631591";
+    export const semaphoreAddress = "0x3889927F0B5Eb1a02C6E2C20b39a1Bd4EAd76131"
+    
--- server/scripts/deployAnonymous.js
+++ server/scripts/deployAnonymous.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+
 const SEMAPHORE_ADDRESS="0x3889927F0B5Eb1a02C6E2C20b39a1Bd4EAd76131";
 
 async function main() {
@@ -8,6 +10,14 @@ async function main() {
   const oneVote = await OneVote.deploy(SEMAPHORE_ADDRESS,1223);
   await oneVote.deployed();
   console.log('OneVote deployed:', oneVote.address);
+
+  fs.writeFileSync(
+    "../clientAnonymousVoting/src/constants/contractAddresses.js",
+    `
+    export const oneVoteAddress = "${oneVote.address}";
+    export const semaphoreAddress = "${SEMAPHORE_ADDRESS}";
+    `
+  );
 }
 
 main()
