e899bfe9a0384c9fdac145209876e20bf47bd8e8
cqlyj <luoyingjie0721@gmail.com>
2024-03-05 12:41:57 +0800

    Refactor import statements and add ParticipationProofs

--- server/contracts/VotingProcess.sol
+++ server/contracts/VotingProcess.sol
@@ -2,21 +2,23 @@
 pragma solidity ^0.8.0;
 // pragma experimental ABIEncoderV2;
 
-import { Semaphore } from './Semaphore.sol';
+import {Semaphore} from "./Semaphore.sol";
+import "./ParticipationProofs.sol";
 
-contract VotingProcess{
+contract VotingProcess {
     uint public id;
     string public name;
     string public description;
     uint startDate;
     uint endDate;
-    
+
+    ParticipationProofs public participationProofs;
+
     bytes[] public proposals;
 
-    mapping (bytes => uint256) public votesPerProposal;
+    mapping(bytes => uint256) public votesPerProposal;
 
     bytes winningProposal;
-
 
     enum Status {
         active,
@@ -30,29 +32,26 @@ contract VotingProcess{
     }
 
     function getStatus() public view returns (Status) {
-
-        if(block.timestamp < startDate) {
+        if (block.timestamp < startDate) {
             return Status.pending;
-        } 
-        
-        else if(block.timestamp < endDate) {
+        } else if (block.timestamp < endDate) {
             return Status.active;
-        } 
-        
-        else {
+        } else {
             return Status.closed;
         }
     }
 
-    function getProposals() view public returns (bytes[] memory){
+    function getProposals() public view returns (bytes[] memory) {
         return proposals;
     }
 
-    function getVotesPerProposal() view public returns (VoteProposal[] memory){
+    function getVotesPerProposal() public view returns (VoteProposal[] memory) {
         uint256 i = 0;
-        VoteProposal[] memory returnVotes = new VoteProposal[](proposals.length);
+        VoteProposal[] memory returnVotes = new VoteProposal[](
+            proposals.length
+        );
 
-        for(i; i < proposals.length; i++){
+        for (i; i < proposals.length; i++) {
             returnVotes[i] = VoteProposal({
                 proposal: proposals[i],
                 numberOfVotes: votesPerProposal[proposals[i]]
@@ -60,35 +59,48 @@ contract VotingProcess{
         }
         return returnVotes;
     }
-    function getWinningProposal() view public returns (VoteProposal memory){
 
-    require(getStatus() == Status.closed, "Results are declared only after the election ends");
+    function getWinningProposal() public view returns (VoteProposal memory) {
+        require(
+            getStatus() == Status.closed,
+            "Results are declared only after the election ends"
+        );
 
         uint256 i = 0;
         VoteProposal memory winningVote;
         winningVote.numberOfVotes = 0;
 
-        for(i; i < proposals.length; i++){
-            if(votesPerProposal[proposals[i]] > winningVote.numberOfVotes){
+        for (i; i < proposals.length; i++) {
+            if (votesPerProposal[proposals[i]] > winningVote.numberOfVotes) {
                 winningVote.proposal = proposals[i];
                 winningVote.numberOfVotes = votesPerProposal[proposals[i]];
             }
         }
         return winningVote;
     }
-    function getStartDate() view public returns (uint){
+
+    function getStartDate() public view returns (uint) {
         return startDate;
     }
-    function getEndDate() view public returns (uint){
+
+    function getEndDate() public view returns (uint) {
         return endDate;
     }
 
     function vote(bytes memory signal) public {
-        require(getStatus() == Status.active, "Election needs to be active to vote");
+        require(
+            getStatus() == Status.active,
+            "Election needs to be active to vote"
+        );
         votesPerProposal[signal] += 1;
+        participationProofs.recordProof(msg.sender, proposals, block.timestamp);
     }
+
     function addProposal(bytes memory _proposal) public {
-        require(getStatus() == Status.pending, "Election needs to be pending to add proposals");
+        require(
+            getStatus() == Status.pending,
+            "Election needs to be pending to add proposals"
+        );
         proposals.push(_proposal);
     }
 
@@ -97,12 +109,14 @@ contract VotingProcess{
         string memory _name,
         string memory _description,
         uint _startDate,
-        uint _endDate
-    ) public{
+        uint _endDate,
+        ParticipationProofs _participationProofs
+    ) public {
         id = _id;
         name = _name;
         description = _description;
         startDate = _startDate;
         endDate = _endDate;
+        participationProofs = _participationProofs;
     }
 }
