99f4faad22f15cf765dc6afb92f09668c4c51cb6
Ronnieraj37 <roshan.singh@soclly.com>
2024-06-19 00:26:46 +0530

    Changed Connect button properties

--- client/app/components/HomePage.tsx
+++ client/app/components/HomePage.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { useAccount } from "wagmi";
 
 const HomePage = () => {
+  const { address, isConnected } = useAccount();
   return (
     <div className="flex p-8 h-screen w-full items-start justify-between">
       Hello
-      <ConnectButton />
+      {isConnected && <p>{address}</p>}
+      <ConnectButton
+        accountStatus={{
+          smallScreen: "avatar",
+          largeScreen: "full",
+        }}
+        showBalance={false}
+      />
     </div>
   );
 };
--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <link rel="icon" href="/aossie.png" sizes="any" />
       <body className={inter.className}>{children}</body>
     </html>
   );
