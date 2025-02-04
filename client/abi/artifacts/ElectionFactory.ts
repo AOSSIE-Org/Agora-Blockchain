ca11648e4174eb0b000896ab1611fac014760408
rairohit17 <154455386+rairohit17@users.noreply.github.com>
2025-02-04 20:46:43 +0530

    Update ElectionFactory abi

--- client/abi/artifacts/ElectionFactory.ts
+++ client/abi/artifacts/ElectionFactory.ts
@@ -1,134 +1,222 @@
 export const ElectionFactory = [
   {
-    inputs: [{ internalType: 'address', name: 'router', type: 'address' }],
-    stateMutability: 'nonpayable',
-    type: 'constructor'
-  },
-  {
-    inputs: [{ internalType: 'address', name: 'router', type: 'address' }],
-    name: 'InvalidRouter',
-    type: 'error'
-  },
-  {
-    inputs: [],
-    name: 'NotWhitelistedSender',
-    type: 'error'
+    inputs: [
+      {
+        internalType: "address",
+        name: "router",
+        type: "address",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "constructor",
   },
   {
     inputs: [],
-    name: 'OnlyOwner',
-    type: 'error'
+    name: "NotWhitelistedSender",
+    type: "error",
   },
   {
     inputs: [],
-    name: 'OwnerRestricted',
-    type: 'error'
+    name: "OnlyOwner",
+    type: "error",
+  },
+  {
+    inputs: [],
+    name: "OwnerRestricted",
+    type: "error",
   },
   {
     anonymous: false,
     inputs: [
-      { indexed: true, internalType: 'bytes32', name: 'messageId', type: 'bytes32' },
-      { indexed: true, internalType: 'uint64', name: 'sourceChainSelector', type: 'uint64' },
-      { indexed: false, internalType: 'address', name: 'sender', type: 'address' }
+      {
+        indexed: true,
+        internalType: "bytes32",
+        name: "messageId",
+        type: "bytes32",
+      },
+      {
+        indexed: true,
+        internalType: "uint64",
+        name: "sourceChainSelector",
+        type: "uint64",
+      },
+      {
+        indexed: false,
+        internalType: "address",
+        name: "sender",
+        type: "address",
+      },
     ],
-    name: 'MessageReceived',
-    type: 'event'
+    name: "MessageReceived",
+    type: "event",
   },
   {
     inputs: [
-      { internalType: 'uint64', name: '_sourceChainSelector', type: 'uint64' },
-      { internalType: 'address', name: '_contractAddress', type: 'address' }
+      {
+        internalType: "uint64",
+        name: "_sourceChainSelector",
+        type: "uint64",
+      },
+      {
+        internalType: "address",
+        name: "_contractAddress",
+        type: "address",
+      },
     ],
-    name: 'addWhitelistedContract',
+    name: "addWhitelistedContract",
     outputs: [],
-    stateMutability: 'nonpayable',
-    type: 'function'
+    stateMutability: "nonpayable",
+    type: "function",
   },
   {
     inputs: [
       {
         components: [
-          { internalType: 'uint256', name: 'startTime', type: 'uint256' },
-          { internalType: 'uint256', name: 'endTime', type: 'uint256' },
-          { internalType: 'string', name: 'name', type: 'string' },
-          { internalType: 'string', name: 'description', type: 'string' }
+          {
+            internalType: "uint256",
+            name: "startTime",
+            type: "uint256",
+          },
+          {
+            internalType: "uint256",
+            name: "endTime",
+            type: "uint256",
+          },
+          {
+            internalType: "string",
+            name: "name",
+            type: "string",
+          },
+          {
+            internalType: "string",
+            name: "description",
+            type: "string",
+          },
         ],
-        internalType: 'struct Election.ElectionInfo',
-        name: '_electionInfo',
-        type: 'tuple'
+        internalType: "struct Election.ElectionInfo",
+        name: "_electionInfo",
+        type: "tuple",
       },
       {
         components: [
-          { internalType: 'uint256', name: 'candidateID', type: 'uint256' },
-          { internalType: 'string', name: 'name', type: 'string' },
-          { internalType: 'string', name: 'description', type: 'string' }
+          {
+            internalType: "uint256",
+            name: "candidateID",
+            type: "uint256",
+          },
+          {
+            internalType: "string",
+            name: "name",
+            type: "string",
+          },
+          {
+            internalType: "string",
+            name: "description",
+            type: "string",
+          },
         ],
-        internalType: 'struct Election.Candidate[]',
-        name: '_candidates',
-        type: 'tuple[]'
+        internalType: "struct Election.Candidate[]",
+        name: "_candidates",
+        type: "tuple[]",
       },
-      { internalType: 'uint256', name: '_ballotType', type: 'uint256' },
-      { internalType: 'uint256', name: '_resultType', type: 'uint256' }
+      {
+        internalType: "uint256",
+        name: "_ballotType",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "_resultType",
+        type: "uint256",
+      },
     ],
-    name: 'createElection',
+    name: "createElection",
     outputs: [],
-    stateMutability: 'nonpayable',
-    type: 'function'
+    stateMutability: "nonpayable",
+    type: "function",
   },
   {
-    inputs: [{ internalType: 'uint256', name: '_electionId', type: 'uint256' }],
-    name: 'deleteElection',
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "_electionId",
+        type: "uint256",
+      },
+    ],
+    name: "deleteElection",
     outputs: [],
-    stateMutability: 'nonpayable',
-    type: 'function'
+    stateMutability: "nonpayable",
+    type: "function",
   },
   {
     inputs: [],
-    name: 'electionCount',
-    outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
-    stateMutability: 'view',
-    type: 'function'
+    name: "electionCount",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
   },
   {
     inputs: [],
-    name: 'factoryOwner',
-    outputs: [{ internalType: 'address', name: '', type: 'address' }],
-    stateMutability: 'view',
-    type: 'function'
+    name: "factoryOwner",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
   },
   {
     inputs: [],
-    name: 'getOpenElections',
-    outputs: [{ internalType: 'address[]', name: '', type: 'address[]' }],
-    stateMutability: 'view',
-    type: 'function'
+    name: "getOpenElections",
+    outputs: [
+      {
+        internalType: "address[]",
+        name: "",
+        type: "address[]",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
   },
   {
-    inputs: [],
-    name: 'getRouter',
-    outputs: [{ internalType: 'address', name: '', type: 'address' }],
-    stateMutability: 'view',
-    type: 'function'
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    name: "openBasedElections",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
   },
   {
-    inputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
-    name: 'openBasedElections',
-    outputs: [{ internalType: 'address', name: '', type: 'address' }],
-    stateMutability: 'view',
-    type: 'function'
-  },
-  {
-    inputs: [{ internalType: 'uint64', name: '_sourceChainSelector', type: 'uint64' }],
-    name: 'removeWhitelistedContract',
+    inputs: [
+      {
+        internalType: "uint64",
+        name: "_sourceChainSelector",
+        type: "uint64",
+      },
+    ],
+    name: "removeWhitelistedContract",
     outputs: [],
-    stateMutability: 'nonpayable',
-    type: 'function'
-  },
-  {
-    inputs: [{ internalType: 'bytes4', name: 'interfaceId', type: 'bytes4' }],
-    name: 'supportsInterface',
-    outputs: [{ internalType: 'bool', name: '', type: 'bool' }],
-    stateMutability: 'pure',
-    type: 'function'
+    stateMutability: "nonpayable",
+    type: "function",
   }
- ] as const;
+] as const;
